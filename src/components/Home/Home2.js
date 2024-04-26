6619ee3be8653bdff6a68760bf98741144672868
SnowsFE <snoerkr@gmail.com>
2024-04-26 16:13:26 +0900

    글로벌 폰트 적용 + Home 글 변경

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -11,9 +11,7 @@ function Home2() {
       <Container>
         <Row>
           <Col md={8} className="home-about-description">
-            <h1 style={{ fontSize: "2.6em" }}>
-              <span className="purple"> Who am i </span>
-            </h1>
+            <h1 style={{ fontSize: "2.6em" }}>{/* <span className="purple"></span> */}</h1>
             <p className="home-about-body">
               React로 웹 정복 하는 그날까지😏
               <br />
@@ -23,13 +21,14 @@ function Home2() {
               가 정말 재미있는 기술 스택이라고 생각합니다.
               <br />
               <br />
-              이러한 기술 스택을 이용하여 많은 사람들이 제 작품을&nbsp;
+              이러한 기술 스택을 이용하여 많은 사람들이 제 사이트를&nbsp;
               <b className="purple">경험</b>하게 하는 것이 목표입니다!
               <br />
               <br />
-              또한, 광고홍보학과를 전공하였기 때문에 어떻게하면 사람들의&nbsp;
-              <b className="purple">Needs</b>를 <br />
-              충족 시킬 수 있을지에 대한 방법을 확실하게 알고 있습니다!
+              저는 광고홍보학과를 전공하였기 때문에 전공 기술로 사람들의&nbsp;
+              <b className="purple">Needs</b>를 쉽게 파악할 수 있습니다.
+              <br />
+              그렇기에 저는 성공한 웹사이트를 제작하는 것에 대한 확신이 있습니다!
             </p>
           </Col>
           <Col md={4} className="myAvtar">
